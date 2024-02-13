ec5a6ce07d85b35b27f68c87bba64f32d879b75f
Vladimir Ghrejyan <vladimir.ghrejyan@gmail.com>
2024-02-13 21:32:53 +0400

    added i18n eslint plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,7 @@ module.exports = {
         'eslint:recommended',
         'plugin:@typescript-eslint/recommended',
         'plugin:react/recommended',
+        'plugin:i18next/recommended',
     ],
     overrides: [
         {
@@ -25,7 +26,7 @@ module.exports = {
         ecmaVersion: 'latest',
         sourceType: 'module',
     },
-    plugins: ['@typescript-eslint', 'react'],
+    plugins: ['@typescript-eslint', 'react', 'i18next'],
     rules: {
         'indent': [2, 4],
         'quotes': ['error', 'single'],
@@ -34,9 +35,6 @@ module.exports = {
         'react/react-in-jsx-scope': 'off',
         'react/no-deprecated': 'warn',
         'no-unused-vars': 'off',
-        '@typescript-eslint/no-unused-vars': [
-            'warn',
-            { argsIgnorePattern: '^_' },
-        ],
+        '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
     },
 };
